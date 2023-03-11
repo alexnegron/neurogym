a6d9a369663f66eccf7f488ba93349eb14644d64
alexnegron <alexnegron18@gmail.com>
2023-03-11 15:25:56 -0500

    context delay rotation tasks

--- neurogym/envs/collections/rotation.py
+++ neurogym/envs/collections/rotation.py
@@ -109,6 +109,7 @@ class _TimeRotation(EnvWithAdditions):
         self.t = t
         self.m = m
         self.rot_comp = rot_comp
+        self.lp = 0
 
 
         # Rewards 
@@ -276,9 +277,216 @@ class _TimeRotation(EnvWithAdditions):
             if self.rot_comp: # comparing rotation speeds; only the fastest-rotating ring rotates mod1 
                 comp = int(self.rot_speed['stim_mod' + str(i+1)] == max(list(self.rot_speed.values())))
             
+
             sum += rot_dirs['stim_mod'+str(i+1)] * m * r * comp
         
         i_target = np.mod(i_thetas['i_theta1'] + sum, self.dim_ring)
+        self.set_groundtruth(i_target, period='decision', where='choice')
+        
+        return trial
+        
+    def _step(self, action): 
+        new_trial = False
+        gt = self.gt_now
+        ob = self.ob_now
+
+        # rewards
+        reward = 0 
+        if self.in_period('fixation'):
+            if action != 0: 
+                new_trial = self.abort
+                reward = self.rewards['abort'] 
+                
+        elif self.in_period('decision'):
+            if action != 0:
+                new_trial = True
+                if action == gt:
+                    reward = self.rewards['correct']
+                    self.performance = 1
+                else:
+                    reward = self.rewards['fail']
+
+        return ob, reward, False, {'new_trial': new_trial, 'gt': gt}
+
+
+class _ContextRotation(EnvWithAdditions):
+    def __init__(self,
+                 dt=100,
+                 rewards=None,
+                 timing=None,
+                 sigma=.15,
+                 dim_ring=16,
+                 stim_mod = (True, True), 
+                 # currently, stim_mod is tuple, turns into dict for arbitrary num of mods
+                 delay = 0,
+                 t = False,
+                 m = False,
+                 rot = (False, False), 
+                 rot_speed = (1, 1),
+                 state = ('fp', 'fp'),
+                 varbump = (False, False),
+                 ctx = 1,
+                 rot_comp = False):
+        
+        super().__init__(dt=dt)
+
+        self.mods = {'stim_mod' + str(i+1) : stim_mod[i] for i in range(len(stim_mod))}
+        self.rot = {'stim_mod' + str(i+1) : rot[i] for i in range(len(rot))}
+        self.rot_speed = {'stim_mod' + str(i+1) : rot_speed[i] for i in range(len(rot_speed))}
+        self.state = {'stim_mod' + str(i+1) : state[i] for i in range(len(state))}
+        self.varbump = {'stim_mod' + str(i+1) : varbump[i] for i in range(len(varbump))}
+        self.dt = dt
+        self.sigma = sigma / np.sqrt(self.dt) # Input noise
+        self.dim_ring = dim_ring
+        self.delay = delay
+        self.t = t
+        self.m = m
+        self.rot_comp = rot_comp
+        self.lp = 0
+        self.ctx = ctx
+
+
+        # Rewards 
+        self.rewards = {'abort':-0.1, 'correct':+1., 'fail':0.}
+        if rewards: 
+            self.rewards.update(rewards) 
+        
+        # Timing
+        self.timing['fixation'] = 500
+        # mod_durs = [np.random.uniform(100, 800) for _ in range(1, len(self.mods))]
+        mod_durs = []
+        #self.timing['stimulus'+str(1)] = 900
+        dur = 500
+        for i in range(len(self.mods)): # skip first ring, set it at end
+            #dur = np.random.uniform(400, 800)
+            mod_durs.append(dur)
+            self.timing['stimulus'+str(i+1)] = dur
+            if self.delay > 0: 
+                if i == len(self.mods.keys())-1:
+                    pass
+                else:
+                    self.timing['delay'+str(i)] = self.delay
+
+        #self.timing['stimulus'+str(1)] = np.max(mod_durs)
+        self.timing['context'] = 200
+        self.timing['decision'] = 200
+        if timing:
+            self.timing.update(timing)
+            
+        self.abort = False
+        self.theta = np.linspace(0, 2*np.pi, self.dim_ring + 1)[:-1] # ignore last, since 2pi = 0 (periodic)
+        self.choices = np.arange(self.dim_ring) 
+        
+    # Spaces 
+        
+        # Observation space
+        name = {'fixation' : 0}
+        
+        # Set up names for each of the stimulus mods
+        for i in range(len(self.mods)): 
+            name['stimulus_mod'+str(i+1)] = range(i*dim_ring + 1, (i+1)*dim_ring + 1)
+        
+        # Set up name for contextual input
+        last = name['stimulus_mod'+str(len(self.mods))][-1]
+        for i in range(len(self.mods)):
+            name['context'+str(i+1)] = 1+last+i
+        
+        num_mods = len(self.mods)
+        self.observation_space = spaces.Box(
+            -np.inf, np.inf,
+            shape = (1 + num_mods*dim_ring + len(self.mods), ), #  + len(self.mods) for the context cues
+            dtype=np.float32,
+            name=name
+        )
+
+        # Action space
+        name = {
+            'fixation': 0,
+            'choice': range(1, dim_ring+1)
+        }
+        self.action_space = spaces.Discrete(1 + dim_ring, name=name)
+    
+    def _add_singlemod(self, trial, mod=1):
+        period = 'stimulus'+str(mod)
+        stim = _gaussianbump(trial['theta' + str(mod)], self.theta, 1) 
+        mod = '_mod' + str(mod) 
+        self.add_ob(stim, period=period, where='stimulus' + mod)
+
+        
+    def _new_trial(self, **kwargs):
+        i_thetas = {'i_theta'+str(i+1) :  self.rng.choice(self.choices) for i in range(len(self.mods))} # Place bumps randomly on each ring
+        
+        # Note: mod1 initial position is random; should this be fixed?  
+        trial = {'theta'+str(i+1) : self.theta[i_thetas['i_theta'+str(i+1)]] for i in range(len(self.mods))}
+        
+        # Timing
+        # note: want each trial to have different timings of rotator ring presentations 
+        mod_durs = []
+        #dur = np.random.uniform(500, 1200)
+        dur = 500
+        if self.delay > 0: 
+            for i in range(len(self.mods)):
+                mod_durs.append(dur)
+                self.timing['stimulus'+str(i+1)] = dur
+                if self.delay > 0: 
+                    if i == len(self.mods.keys())-1:
+                        pass
+                    else:
+                        self.timing['delay'+str(i)] = self.delay
+        else:
+            self.timing['stimulus'] = dur
+            
+        # Periods 
+        periods = list(self.timing.keys())
+        self.add_period(periods)
+
+        # Add rotating rings 
+        for i in range(len(self.mods)): 
+            if self.mods['stim_mod'+str(i+1)]: 
+                num = i+1
+                self._add_singlemod(trial, mod=num)
+        
+        # perform task-prescribed modifications to each ring 
+        rot_dirs = {} # keep track of which directions ring rotates
+        for i in range(len(self.mods)):
+            #dir = np.random.choice([-1,1])
+            dir = 1 
+            rot_dirs['stim_mod'+str(i+1)] = dir
+            speed = self.rot_speed['stim_mod'+str(i+1)]
+            obs = self.ob[self.start_ind['stimulus'+str(i+1)] : self.end_ind['stimulus'+str(i+1)]]
+            lp_idx = i_thetas['i_theta'+str(i+1)] # bump idx tracker (used for lp tasks)
+            dir = rot_dirs['stim_mod'+str(i+1)]
+
+            
+            for j in range(obs.shape[0]):
+                if self.rot['stim_mod'+str(i+1)]: # mod is rotating
+                    obs[j, i*self.dim_ring+1 : (i+1)*self.dim_ring+1] = np.roll(obs[j, i*self.dim_ring+1 : (i+1)*self.dim_ring+1], dir*speed*j) # performs rotation
+            
+            prev = i_thetas['i_theta'+str(i+1)]
+            i_thetas['i_theta'+str(i+1)] = np.mod(prev + dur//self.dt-1, self.dim_ring)
+
+        self.add_ob(1, where='fixation')
+        self.set_ob(0, 'decision')
+        self.set_ob(0, period='fixation', where='stimulus_mod1')
+
+        # add noise 
+        self.add_randn(0, self.sigma, ['stimulus'+str(i+1) for i in range(len(self.mods))])
+        if self.delay > 0: 
+            self.add_randn(0, self.sigma, ['delay'+str(i) for i in range(1, len(self.mods) - 1)])
+        
+        #ctx_idx = np.random.choice(1,range(len(self.mods)+1))
+        self.set_ob(0, period='context')
+        self.set_ob(1, period='context', where='context'+str(self.ctx))
+
+        for i in range(len(self.mods)):
+            for j in range(len(self.mods)):
+                self.set_ob(0, period='stimulus'+str(i+1), where='context'+str(j+1))
+
+        # self.pos = i_thetas['i_theta'+str(ctx_idx+1)]
+        # self.ctx = ctx_idx 
+        i_target = i_thetas['i_theta'+str(self.ctx)] # target is determined by context
+        self.i_target = i_target
+        
         self.set_groundtruth(i_target, period='decision', where='choice')
         
         return trial
@@ -328,6 +536,10 @@ def _dlytrot_kwargs():
     env_kwargs = {'delay' : 300} # default delay length is 300ms 
     return env_kwargs
 
+def _2ring_ctxrot_kwargs():
+    env_kwargs = {'stim_mod':(True, True),'rot':(True, True), 'state':('lp','lp'), 'sigma':0}
+    return env_kwargs
+
 """
 Task naming conventions
 
@@ -349,6 +561,25 @@ Example 2:
 
 """
 ##########
+def ctxrot(**kwargs):
+    env_kwargs = _2ring_ctxrot_kwargs()
+    return _ContextRotation(**env_kwargs)
+
+def dlyctxrot1(**kwargs):
+    env_kwargs = _2ring_ctxrot_kwargs()
+    env_kwargs.update({'delay':500, 'ctx':1})
+    return _ContextRotation(**env_kwargs)
+
+def dlyctxrot2(**kwargs):
+    env_kwargs = _2ring_ctxrot_kwargs()
+    env_kwargs.update({'delay':500, 'ctx':2})
+    return _ContextRotation(**env_kwargs)
+
+
+def ctxrot2(**kwargs): 
+    pass
+
+
 
 """
 Fixed ring hierachy 1. 
